e12197cdba3c7be562f966e6759f563b8f1cd220
haoxuan <changhoehin2005@gmail.com>
2023-05-09 22:48:28 +0800

    rearrange

--- crisscross.py
+++ crisscross.py
@@ -1,10 +1,18 @@
 import random
-def grid():
+def gridnNum():
     for row in range(6):
         print("--------")
         for column in range(6):
-            print("|",end = " ")
-            print(random.randint(-30,31),end = " ")
+            print("|",end = "")
+            x = random.randint(-30,31)
+            if x >= 0 and x < 10:
+                print(f"  {x}  ",end = "")
+            elif x >= 10:
+                print(f" {x}  ",end= '')
+            elif x < 0 and x > -10:
+                print(f" {x}  ", end = '')
+            else:
+                print(f" {x} ",end = '')
             if column == 5 :
                 print("|")    
-grid()
+gridnNum()
