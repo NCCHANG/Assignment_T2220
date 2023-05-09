e11095244d1d11617839dbff06f63676aef87f18
Ansonlim2311 <ansonlim2311@gmail.com>
2023-05-09 22:32:10 +0800

    hi

--- crisscross.py
+++ crisscross.py
@@ -1,7 +1,10 @@
+import random
 def grid():
     for row in range(6):
         print("--------")
         for column in range(6):
             print("|",end = " ")
-        print()
+            print(random.randint(-30,31),end = " ")
+            if column == 5 :
+                print("|")    
 grid()
