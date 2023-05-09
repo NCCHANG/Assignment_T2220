370854df6401baaedbade9382360632f85f77d21
haoxuan <changhoehin2005@gmail.com>
2023-05-10 00:05:11 +0800

    changed a lot lol

--- crisscross.py
+++ crisscross.py
@@ -2,24 +2,24 @@ import random
 def gridnNum():
     randomNum = []
     size = int(input('Enter Grid Size: '))
+    i = 0
+    while len(randomNum) != size * size: #append all random number
+        x = random.randint(-30,30)
+        randomNum.append(x)
     for row in range(size):
         print("------" * size,end = "")
         print("-")
         for column in range(size):
             print("|",end = "")
-            x = random.randint(-30,30)
-            randomNum.append(str(x))
-            if len(randomNum) != size * size :
-                continue
-            #print(randomNum)
-            if x >= 0 and x < 10:
-                print(f"  {x}  ",end = "")
-            elif x >= 10:
-                print(f" {x}  ",end= '')
-            elif x < 0 and x > -10:
-                print(f" {x}  ", end = '')
+            if randomNum[i] >= 0 and randomNum[i] < 10:
+                print(f"  {randomNum[i]}  ",end = "")
+            elif randomNum[i] >= 10:
+                print(f" {randomNum[i]}  ",end= '')
+            elif randomNum[i] < 0 and randomNum[i] > -10:
+                print(f" {randomNum[i]}  ", end = '')
             else:
-                print(f" {x} ",end = '')
+                print(f" {randomNum[i]} ",end = '')
+            i += 1                 #to loop through list
             if column == size-1 :
                 print("|")
     print("------" * size,end = "")
